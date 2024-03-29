8feb7b7b7fc34f3d271640789aa1bbd7b37b0491
Ba9A-tata <yogadzakkii@gmail.com>
2024-03-29 15:42:22 +0700

    menambahkan step 13

--- ALGO/ALGO.cpp
+++ ALGO/ALGO.cpp
@@ -79,4 +79,8 @@ void q_short(int low, int high)
 	//recursive call to sort the left sub aaray
 	q_short(low, j - 1); //step12
 
+	//recursice call to sort the right sub array
+
+	q_short(j + 1, high); //step 13
+
 	
